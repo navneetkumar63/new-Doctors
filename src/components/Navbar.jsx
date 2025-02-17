fe829d5c2e070ed7cc876281f962783ddabc57ce
navneet kumar <navneetrajput7505@gmail.com>
2025-02-17 13:10:42 +0530

    day17

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,19 +15,19 @@ const Navbar = () => {
         <ul className='hidden md:flex items-start gap-5 font-medium' >
             <NavLink to='/'>
                 <li className='py-1'>HOME</li>
-                <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden'/>
+                <hr className='border-none outline-none h-0.5 bg-pink-600 w-3/5 m-auto hidden'/>
             </NavLink>
             <NavLink to='/doctors'>
                 <li className='py-1'>ALL DOCTORS</li>
-                <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto  hidden' />
+                <hr className='border-none outline-none h-0.5 bg-pink-600 w-3/5 m-auto  hidden' />
             </NavLink>
             <NavLink to='/about'>
                 <li className='py-1'>ABOUT</li>
-                <hr className='border-none outline-none h-0.5 bg-primary  w-3/5 m-auto  hidden' />
+                <hr className='border-none outline-none h-0.5 bg-pink-600  w-3/5 m-auto  hidden' />
             </NavLink>
             <NavLink to='/contact'>
                 <li className='py-1'>CONTACT</li>
-                <hr className='border-none outline-none h-0.5 bg-primary  w-3/5 m-auto  hidden'/>
+                <hr className='border-none outline-none h-0.5 bg-pink-600  w-3/5 m-auto  hidden'/>
             </NavLink>
 
         </ul>
@@ -37,7 +37,7 @@ const Navbar = () => {
                 <img className='w-2.5' src={assets.dropdown_icon}/>
              
               <div className='absolute top-0 right-0 pt-14 text-base font-medium text-grey-600 z-20 hidden group-hover:block'>
-                <div className='min-w-48 bg-stone-100 rounded flex flex-col gap-4 p-4'>
+                <div className='min-w-48 bg-stone-100 rounded flex flex-col gap-4 p-4'>--
                 <p onClick={()=>navigate('my-profile')} className='hover:text-black cursor-pointer'>My Profile</p>
                 <p onClick={()=>navigate('my-appoitments')} className='hover:text-black cursor-pointer'>My Appointment</p>
                 <p onClick={()=>setToken(false)} className='hover:text-black cursor-pointer'>Logout
@@ -48,6 +48,21 @@ const Navbar = () => {
             </div>
             :<button onClick={()=>navigate('/login')} className='bg-pink-400 text-white px-8 py-3 rounded-full font-light hidden md:block '>Create Account</button>
          }
+         <img onClick={()=>setShowMenu(true)} className='w-6 md:hidden' src={assets.menu_icon}/>
+
+         {/*-----------mobile menu------------*/}
+         <div className={` ${showMenu ? 'fixed w-full' :'h-0 w-0'} md:hidden right-0 top-0 bottom-0 z-20 overflow-hidden bg-white transition-all` }>
+            <div className='flex items-center justify-between px-5 py-6'>
+                <img className='w-36' src={assets.logo}/>
+                <img  className='w-7'  onClick={()=>setShowMenu(false)} src={assets.cross_icon}/>
+            </div>
+            <ul className='flex flex-col items-center gap-2 mt-5 px-5 text-lg font-medium'>
+            <NavLink  onClick={()=>setShowMenu(false)} to='/'><p className='px-4 py-2 rounded inline-block'>Home</p></NavLink>
+               <NavLink  onClick={()=>setShowMenu(false)} to='/doctors'><p className='px-4 py-2 rounded inline-block'>All Doctors</p></NavLink>
+               <NavLink  onClick={()=>setShowMenu(false)} to='/about'><p className='px-4 py-2 rounded inline-block'>About</p></NavLink>
+               <NavLink  onClick={()=>setShowMenu(false)} to='/contact'><p className='px-4 py-2 rounded inline-block'>Contact</p></NavLink>
+            </ul>
+         </div>
           </div>
     </div>
   )
